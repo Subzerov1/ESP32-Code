2f5372e19cbc5dc884d4f1aa1b5a09bf5b697d0f
Youssef Refaat <300code.yossifrefaat@gmail.com>
2026-01-15 23:54:21 +0200

    update software

--- project_3_code.ino
+++ project_3_code.ino
@@ -2,14 +2,27 @@
 #include <Adafruit_AMG88xx.h>
 #include <TFT_eSPI.h>
 #include <SPI.h>
+// #define targetTemp 15
+#define targetTemp  15
+#define pixelSize 10
+#define blackColor 0x0000
+#define whiteColor 0xFFFF
+#define verticalTextPadding 10
+#define horizentalTextPadding 10
+#define sensorPixelsSize 8
+#define mapTopPadding 33
+#define screenWidth 240
+#define screenHight 320
+
+
 
 Adafruit_AMG88xx amg;
 TFT_eSPI tft = TFT_eSPI(); 
 
 
 float pixels[64];
-int pixelSize = 10;
 uint16_t backfroundColor = tft.color565(157, 255, 175);
+float lastPersentage = -1;
 
 void setup() {
   Serial.begin(115200);
@@ -32,26 +45,32 @@ void setup() {
 void loop() {
   amg.readPixels(pixels);
 
-  for (int i = 0; i < 64; i++) {
-    Serial.print(pixels[i]);
-    Serial.print(" , ");
-    if ((i + 1) % 8 == 0) Serial.println();
+  int targetPixelsCount = 0;
+  for(int i = 0 ; i < 64 ; i++){
+    if(pixels[i] < targetTemp) {
+      targetPixelsCount++;
+    }
   }
-
-  // String text = "Max: ";
-  // text += String(maxValue(pixels), 1); // رقم عشري واحد
-  // text += "  , ";
-  // text += "Min: ";
-  // text += String(minValue(pixels), 1);
-
-  // writeString(text, 10, 10);
-  // drawThermalMap(pixels);
-  // 30
-  drawThermalMapSmooth(pixels, 240 , 320);
-  // delay(500);
+  float persentage = ((float) targetPixelsCount / 64.0) * 100.0;
+  drawPersentage(persentage);
+  drawThermalMapSmooth(pixels, screenWidth , (screenHight - mapTopPadding));
+  // delay(30);
 }
 
-
+void drawPersentage(float persentage) {
+  if(lastPersentage == persentage) return;
+  int textHight = 16;
+  Serial.println(textHight);
+  tft.fillRect(0 , 0 , tft.width() , (textHight + verticalTextPadding) , blackColor);
+  tft.fillRect(0 , (textHight + verticalTextPadding + 5) , tft.width() , 2 , tft.color565(137, 137, 137) );
+  tft.setTextSize(2.7);
+  tft.setTextColor(whiteColor); 
+  tft.setTextDatum(TL_DATUM);
+  tft.drawString("SUBZERO", horizentalTextPadding , verticalTextPadding , 1);
+  tft.setTextDatum(TR_DATUM);
+  tft.drawString(String(persentage,2) + "%", (tft.width() - horizentalTextPadding) ,  verticalTextPadding, 1);
+  lastPersentage = persentage;
+}
 
 float maxValue(float values[64]) {
   float max = values[0] ;
@@ -94,46 +113,39 @@ void drawThermalMap(float values[64]) {
 
 
 uint16_t thermalColor(float temp, float tMin, float tMax) {
-    float ratio = (temp - tMin) / (tMax - tMin);
-    ratio = constrain(ratio, 0, 1);
-
     uint8_t r, g, b;
-
-    if (ratio <= 0.25) { r=0; g=4*ratio*255; b=255; }               // أزرق → سماوي
-    else if (ratio <= 0.5) { r=0; g=255; b=255 - 4*(ratio-0.25)*255;} // سماوي → أخضر
-    else if (ratio <= 0.75) { r=4*(ratio-0.5)*255; g=255; b=0;}     // أخضر → أصفر
-    else { r=255; g=255 - 4*(ratio-0.75)*255; b=0;}                 // أصفر → أحمر
-
+    if(temp < targetTemp) { r=48; g=92; b= 222; } 
+    else { r = 0; g = 0; b = 0; }
     return tft.color565(r,g,b);
 }
 
+
+
 void drawThermalMapSmooth(float values[64], int mapWidth, int mapHeight) {
-  int smallSize = 8; // المصفوفة الأصلية 8×8
   float tMin = minValue(values);
   float tMax = maxValue(values);
   for (int y_big = 0; y_big < mapHeight; y_big++) {
     for (int x_big = 0; x_big < mapWidth; x_big++) {
       // نحدد موقع البكسل بالنسبة للصورة الصغيرة
-      float x_small = x_big * (float)(smallSize-1) / (mapWidth-1);
-      float y_small = y_big * (float)(smallSize-1) / (mapHeight-1);
+      float x_small = x_big * (float)(sensorPixelsSize-1) / (mapWidth-1);
+      float y_small = y_big * (float)(sensorPixelsSize-1) / (mapHeight-1);
 
       int x0 = floor(x_small);
-      int x1 = min(x0 + 1, smallSize-1);
+      int x1 = min(x0 + 1, sensorPixelsSize-1);
       int y0 = floor(y_small);
-      int y1 = min(y0 + 1, smallSize-1);
+      int y1 = min(y0 + 1, sensorPixelsSize-1);
 
       float dx = x_small - x0;
       float dy = y_small - y0;
 
       // interpolationdy
-      float temp = (1-dx)*(1-dy)*values[y0*smallSize + x0] +
-                   dx*(1-dy)*values[y0*smallSize + x1] +
-                   (1-dx)*dy*values[y1*smallSize + x0] +
-                   dx*dy*values[y1*smallSize + x1];
+      float temp = (1-dx)*(1-dy)*values[y0*sensorPixelsSize + x0] +
+                   dx*(1-dy)*values[y0*sensorPixelsSize + x1] +
+                   (1-dx)*dy*values[y1*sensorPixelsSize + x0] +
+                   dx*dy*values[y1*sensorPixelsSize + x1];
 
       uint16_t color = thermalColor(temp, tMin, tMax);
-      tft.drawPixel(x_big, y_big, color); // رسم كل بكسل
-
+      tft.drawPixel(x_big, (y_big + mapTopPadding), color); // رسم كل بكسل
     }
   }
 }
